4d869d5dff6059f94a2308861d391c58ebafa93e
Michaël <cornelismichael@hotmail.com>
2023-03-04 16:47:05 +0100

    First work on sidebar

--- script/index.js
+++ script/index.js
@@ -8,3 +8,12 @@ subheaderItems.forEach(item => {
   item.classList.add('active');
 });
 });
+
+const sidebarItems = document.querySelectorAll('.sidebar-item');
+
+sidebarItems.forEach(item => {
+  item.addEventListener('click', () => {
+  sidebarItems.forEach(item => item.classList.remove('active'));
+  item.classList.add('active');
+});
+});
